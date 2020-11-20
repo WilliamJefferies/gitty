13e5c22e54dfa25359ba7ea13fd4570f3fa29eb0
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-11-20 11:44:57 +1100

    checks to see if cwd is a git repo

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const gitty = require('commander');
-
-const chalk = require('chalk');
-const figlet = require('figlet');
-const clear = require('clear');
-
-const files = require('./lib/files');
+const gitty = require("commander");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const clear = require("clear");
+const files = require("./lib/files");
 
 gitty
-    .command('init')
-    .description('Draw app banner')
-    .action(() => {
-        clear();
-        console.log(chalk.magenta(figlet.textSync('gitty', {horizontalLayout: 'full'})));
-    });
+  .command("init")
+  .description("Draw app banner")
+  .action(() => {
+    clear();
+    console.log(
+      chalk.magenta(figlet.textSync("gitty", { horizontalLayout: "full" }))
+    );
+  });
 
 gitty.parse(process.argv);
-if(!gitty.args.length) {
-    gitty.help();
+if (!gitty.args.length) {
+  gitty.help();
 }
