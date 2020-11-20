0f4e0a2f61c86ee0dd3f5beaf52889eb29b1ce02
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-11-20 12:31:28 +1100

    implements github token

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const gitty = require("commander");
 const chalk = require("chalk");
 const figlet = require("figlet");
 const clear = require("clear");
+
 const files = require("./lib/files");
+const github = require("./lib/github_credentials");
 
 gitty
   .command("init")
@@ -12,6 +14,18 @@ gitty
     console.log(
       chalk.magenta(figlet.textSync("gitty", { horizontalLayout: "full" }))
     );
+  });
+
+gitty
+  .command("octocheck")
+  .description("Check user GitHub credentials")
+  .action(async () => {
+    let token = github.getStoredGitHubToken();
+    if (!token) {
+      await github.setGitHubCredentials();
+      token = await github.registerNewToken();
+    }
+    console.log(token);
   });
 
 gitty.parse(process.argv);
